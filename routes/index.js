43ef5f374e27108b5df9637c783787de3503009b
Vishal Vashishth <vishal112vp@gmail.com>
2026-07-04 22:15:41 +0530

    mongoodb'

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require("./users")
+const userModel = require("./users");
 
 router.get("/", function(req, res){
   res.render("index");
-})
-router.get("/create", function(req, res){
-  const newUser = userModel({
-    user: "JohnDoe",
-    email: "john.doe@example.com",
-    name: "John Doe",
-    age: 21
-  });
-  res.send(newUser);
+});
+
+// router.get("/create", function(req, res){
+//   const newUser = userModel({
+//     user: "JohnDoe",
+//     email: "john.doe@example.com",
+//     name: "John Doe",
+//     age: 21
+//   });
+//   res.send(newUser );
+// });
+
+// router.get("/allUsers",  async function(req, res){
+//   let allUsers = await userModel.findOne({user: "JohnDoe"});
+//   res.send(allUsers);
+// });
+
+router.get("/delete", async function(req, res){
+  let deletedUser = await userModel.findOneAndDelete({user: "JohnDoe"});
+  res.send(deletedUser);
 });
 
 module.exports = router;

dab54c958d84b6fbb8745de3b8e5eceae1d85ea6
Vishal Vashishth <vishal112vp@gmail.com>
2026-07-06 22:28:49 +0530

    minor change

--- mongo/index.js
+++ mongo/index.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
+main()
+    .then(() => {
+    console.log("Connected to MongoDB");
+})
+main().catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
 
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/mydatabase");
 }
-main();
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+const User = mongoose.model("User", userSchema);
